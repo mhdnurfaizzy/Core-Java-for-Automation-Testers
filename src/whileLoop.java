b27e25959c077613bdc5a96d44b83ffcf868551f
mhdnurfaizzy <mhdnurfaizzy@gmail.com>
2024-05-04 10:12:32 +0700

    train sumOfDigits with whileLoop

--- src/whileLoop.java
+++ src/whileLoop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class whileLoop {
 
 
@@ -17,12 +19,42 @@ public class whileLoop {
         } while (j>30);
     }
 
+    public void count() {
+        int i = 1;
+        while(i<=5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public void sumOfDIgits() {
+
+        int number, digit, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        number = sc.nextInt();
+        while(number > 0)
+        {
+//finds the last digit of the given number
+            digit = number % 10;
+//adds last digit to the variable sum
+            sum = sum + digit;
+//removes the last digit from the number
+            number = number / 10;
+        }
+//prints the result
+        System.out.println("Sum of Digits: "+sum);
+    }
+
+
 
 
     public static void main(String[] args) {
         whileLoop wl = new whileLoop();
         wl.whileLoop();
         wl.doWhileloop();
+        wl.count();
+        wl.sumOfDIgits();
 
     }
 }
