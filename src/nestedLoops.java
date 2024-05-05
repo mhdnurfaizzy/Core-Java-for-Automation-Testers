18409ee558ec76ba0e3b3f5f260620a49f30708c
mhdnurfaizzy <mhdnurfaizzy@gmail.com>
2024-05-05 14:36:06 +0700

    exercise pyramid nestedloop

--- src/nestedLoops.java
+++ src/nestedLoops.java
@@ -1,6 +1,36 @@
 import java.util.Scanner;
 
 public class nestedLoops {
+    public void otherPyramid() {
+
+        int k=1;
+
+        for(int i=1;i<4;i++) {
+            for(int j=1;j<=4-i;j++) {
+                System.out.print(k);
+                System.out.print("\t");
+                k++;
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+    public void otherPyramid2() {
+
+        int k=1;
+
+        for(int i=1;i<4;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(k);
+                System.out.print("\t");
+                k++;
+            }
+            System.out.println(" ");
+        }
+
+    }
+
     public void pyramidNestedLoops() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number : ");
@@ -14,7 +44,7 @@ public class nestedLoops {
             }
             // loop to print the number of stars in each row
             for (int k=1; k <= i;k++) {
-                System.out.println("*");
+                System.out.print("*");
             }
             // for new line after printing each row
             System.out.println();
@@ -25,6 +55,12 @@ public class nestedLoops {
     public static void main(String[] args) {
         nestedLoops nl = new nestedLoops();
         nl.pyramidNestedLoops();
+
+        System.out.println();
+
+        nl.otherPyramid();
+        System.out.println();
+        nl.otherPyramid2();
 
         for (int i=1;i<=3;i++) { //looping for 3 times
             System.out.println("Outer loop started");
