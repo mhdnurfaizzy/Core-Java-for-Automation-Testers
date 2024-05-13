96932ca92412469078e41a477d37da9ab96e57ce
JIH <mhdnurfaizzy@yahoo.com>
2024-05-13 11:32:31 +0700

    interface example with traffic rules

---
+++ src/Interface/IndonesiaTraffic.java
@@ -0,0 +1,48 @@
+package Interface;
+
+public class IndonesiaTraffic implements CentralTraffic, SymbolTraffic{
+
+    public void walkSymbol() {
+        System.out.println("Walk in pedestrian");
+    }
+
+    //CentralTraffic Interface
+    @Override
+    public void greenGo() {
+        System.out.println("GreenGo implementations");
+    }
+
+    @Override
+    public void redStop() {
+        System.out.println("redStop implementations");
+
+    }
+
+    @Override
+    public void flashYellow() {
+        System.out.println("flashYellow implementations");
+
+    }
+
+    //SymbolTraffic
+    @Override
+    public void stopSymbol() {
+        System.out.println("stopSymbol implementations");
+
+    }
+
+    public static void main(String[] args) {
+        CentralTraffic  cit = new IndonesiaTraffic();
+        cit.greenGo();
+        cit.redStop();
+        cit.flashYellow();
+
+        IndonesiaTraffic it = new IndonesiaTraffic();
+        it.walkSymbol();
+
+        SymbolTraffic st = new IndonesiaTraffic();
+        st.stopSymbol();
+    }
+
+
+}
---
+++ src/Interface/SymbolTraffic.java
@@ -0,0 +1,6 @@
+package Interface;
+
+public interface SymbolTraffic {
+
+    public void stopSymbol();
+}
