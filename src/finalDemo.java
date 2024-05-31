c6455376a10082b4c1950b85a46cca6606853864
JIH <mhdnurfaizzy@yahoo.com>
2024-05-31 17:16:00 +0700

    finalDemo

---
+++ src/finalDemo.java
@@ -0,0 +1,13 @@
+public class finalDemo {
+
+    /*
+        final void getData(){ // cant call using extends
+
+        }
+     */
+
+    public static void main(String[] args) {
+        final  int i=4; //constant value cant change
+
+    }
+}
